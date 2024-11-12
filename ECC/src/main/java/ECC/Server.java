854091659fc0cf614ea919866f6f71feee0111a8
TarsisiusRisto <risto.tarsisius@gmail.com>
2024-11-12 16:45:14 +0700

    hah

--- ECC/src/main/java/ECC/Server.java
+++ ECC/src/main/java/ECC/Server.java
@@ -16,8 +16,8 @@ import java.util.Base64;
 public class Server {
 
     private static final int PORT = 5001;
-    private static final String KEY_SERVER_ADDRESS = "localhost";
-    // private static final String KEY_SERVER_ADDRESS = "172.31.42.54"; // region Tokyo
+    // private static final String KEY_SERVER_ADDRESS = "localhost";
+    private static final String KEY_SERVER_ADDRESS = "172.31.42.54"; // region Tokyo
     // private static final String KEY_SERVER_ADDRESS = "11.0.13.26"; // Region Ohio
     // private static final String KEY_SERVER_ADDRESS = "20.0.2.201"; // Region Singapore
     // private static final String KEY_SERVER_ADDRESS = "192.168.1.5"; // Ethernet
